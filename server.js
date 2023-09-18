fec7d18f9ec268eec0482a3c360bb36a30d9b6df
ikarthik21 <karthikeyakallapu@gmail.com>
2023-09-18 10:10:24 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+import dotenv from 'dotenv';
+import { Server } from 'socket.io'
+dotenv.config();
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: process.env.FRONTEND_URL,
+        methods: ["GET", "POST"],
+    },
+});
+
+
+
+console.log(process.env.FRONTEND_URL);
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+});
+
+
+app.get('/pingscript', (req, res) => {
+    const { id } = req.query;
+    io.emit("id_receive", id);
+    return res.status(200).json({ message: "ID has been sent" });
+})
+
+
+
+server.listen(port, () => {
+    console.log("SERVER IS RUNNING", port);
+});
